2bb78aa97f0724d9e7a5a6c96e1750d2eae3949d
Braulio Cassule <braulio.cassule@bancobai.ao>
2019-11-28 15:55:01 +0100

    add mux router

---
+++ controllers/project_controller.go
@@ -0,0 +1,7 @@
+package controllers
+
+import "net/http"
+
+var GetProject = func(w http.ResponseWriter, r *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"github.com/braulio94/base_project/controllers"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+)
+
 func main() {
-	
+	router := mux.NewRouter()
+	router.HandleFunc("/api/project", controllers.GetProject).Methods("GET")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" //localhost
+	}
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Println("Serving on port", port)
 }
